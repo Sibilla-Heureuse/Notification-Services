aedbaf8a59b3e713ff4ed46e8421693ec3cfd2b9
Sibilla-Heureuse <sibillaheureuse@gmail.com>
2026-02-19 07:01:59 +0200

    commit NotificatioService with full implementation with main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     public static void main(String[] args) {
 
+        String emailMessage = "Welcome to our system via Email!";
+        String smsMessage = "Your OTP is 123456.";
+        String pushMessage = "You have a new app alert!";
 
+
+        NotificationService emailService = new EmailNotification();
+        emailService.sendNotification(emailMessage);
+
+
+        NotificationService smsService = new SMSNotification();
+        smsService.sendNotification(smsMessage);
+
+
+        NotificationService pushService = new PushNotification("DEVICE-001");
+        pushService.setDefaultPriority("HIGH");
+        pushService.sendNotification(pushMessage);
     }
 }
